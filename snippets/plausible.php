2aa8a654203b053d49d282b435eef068fdd9a85a
Tobias Battenberg <mountbatt@gmail.com>
2023-09-25 10:14:16 +0200

    update snippet

--- snippets/plausible.php
+++ snippets/plausible.php
@@ -1,3 +1,5 @@
-<?php if(option('debug') !== true && !kirby()->user()): ?>
+<?php if(option('debug') !== true && !kirby()->user() && option('mountbatt.plausible.plausibleURL')): ?>
 <script defer data-domain="<?= option('mountbatt.plausible.domain') ?? parse_url($kirby->url('index'))['host'] ?>" src="<?= option('mountbatt.plausible.plausibleURL') ?>"></script>
+<?php else: ?>
+  <!-- Plausible not loaded cause you are logged in at the panel or there is no option "plausibleURL" -->
 <?php endif; ?>
